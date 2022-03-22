db16290d54725b93d37477abbd5cc07ac28680de
DRAGANA-PC\Dragana <natasa.vasic92@gmail.com>
2022-03-22 20:11:54 +0100

    no message

---
+++ sikuliX/workshope.sikuli/workshope.py
@@ -0,0 +1,59 @@
+click ("1647544225816.png")
+wait(1)
+click ("1647882355022.png")
+wait(1)
+type ("SikuliX Documentation"); type(Key.ENTER)
+click("1647882873967.png")
+wait(1)
+Mouse.wheel(WHEEL_DOWN, 7)
+wait(1)
+Mouse.wheel(WHEEL_DOWN, 7)
+wait(1)
+Mouse.wheel(WHEEL_DOWN, 7)
+wait(1)
+Mouse.wheel(WHEEL_DOWN, 7)
+wait(1)
+Mouse.wheel(WHEEL_DOWN, 7)
+wait(1)
+Mouse.wheel(WHEEL_DOWN, 7)
+wait(1)
+Mouse.wheel(WHEEL_DOWN, 7)
+click("1647883867156.png")
+wait(1)
+Mouse.wheel(WHEEL_DOWN, 10)
+wait(1)
+Mouse.wheel(WHEEL_DOWN, 10)
+wait(1)
+Mouse.wheel(WHEEL_DOWN, 10)
+wait(1)
+click("1647891927819.png")
+wait(2)
+click("1647892218339.png")
+wait(1)
+click("1647892267748.png")
+wait(1)
+type("https://www.tutorialspoint.com/batch_script/batch_script_copy.htm"); type(Key.ENTER)
+Mouse.wheel(WHEEL_DOWN, 10)
+wait(1)
+click("1647892640533.png")
+rightClick("1647892851913.png")
+click("1647893008079.png")
+click("1647893441064.png")
+type("zaKorpiranje"); type(Key.ENTER)
+rightClick("1647893198415.png")
+click("1647893232635.png")
+click("1647893450665.png")
+type("skriptaZaKopiranjeFajla"); type(Key.ENTER)
+wait(2)
+
+
+
+
+
+
+
+
+
+
+
+
